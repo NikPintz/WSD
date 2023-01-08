d2d2fd3b5f711aaa278fbf97ae4d21684655d15a
NikhilPintoCU <116797770+NikhilPintoCU@users.noreply.github.com>
2023-01-08 13:19:22 +0530

    Add files via upload

---
+++ lLab10/lab10db.js
@@ -0,0 +1,15 @@
+
+const pool = createPool({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "test",
+  connectionLimit: 10
+})
+
+pool.query(`select * from registration`, function(err, result, fields) {
+  if (err) {
+      return console.log(err);
+  }
+  return console.log(result);
+})
